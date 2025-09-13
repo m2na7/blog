a0a92cb0bf7e3974bc3106c4b1c01c32d4d1ed89
Minha Kang <118591632+m2na7@users.noreply.github.com>
2025-09-14 00:07:17 +0900

    feat: add about-me-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-import Link from 'next/link'
-
-import PostCardHero from '@/components/post-card-hero'
-import Title from '@/components/title'
+import AboutMeSection from '@/components/about-me-section'
+import RecentPostSection from '@/components/recent-post-section'
 import { getAllPosts } from '@/lib/posts'
 
 export default function HomePage() {
@@ -9,39 +7,10 @@ export default function HomePage() {
   const recentPosts = allPosts.slice(0, 3)
 
   return (
-    <div className="space-y-12">
+    <div className="space-y-12 max-sm:space-y-8">
+      <AboutMeSection />
       {recentPosts.length > 0 && (
-        <section>
-          <div className="mb-2 flex items-center justify-between">
-            <div>
-              <Title size="lg" className="mb-4">
-                최근 포스트
-              </Title>
-              <p className="mt-2 text-gray-600 dark:text-gray-400">
-                새롭게 작성된 포스트들을 확인해보세요.
-              </p>
-            </div>
-            <Link
-              href="/posts"
-              className="group mt-3 flex items-center space-x-2 rounded-full border border-gray-200 bg-white px-4 py-2 text-sm font-medium text-gray-700 transition-all hover:bg-gray-50 dark:border-zinc-700 dark:bg-zinc-800 dark:text-gray-300 dark:hover:bg-zinc-700"
-            >
-              <span>전체 보기</span>
-              <span className="transition-transform group-hover:translate-x-1">
-                →
-              </span>
-            </Link>
-          </div>
-
-          <div className="grid gap-8">
-            {recentPosts.map((post, index) => (
-              <PostCardHero
-                key={post.slug}
-                post={post}
-                className={index === 0 ? 'md:col-span-2' : ''}
-              />
-            ))}
-          </div>
-        </section>
+        <RecentPostSection recentPosts={recentPosts} />
       )}
     </div>
   )
---
+++ src/components/about-me-section.tsx
@@ -0,0 +1,88 @@
+import Image from 'next/image'
+
+export default function AboutMeSection() {
+  return (
+    <section className="space-y-8 border-b border-gray-200 pb-8 dark:border-zinc-700">
+      <Image
+        src="/assets/m2na_profile.png"
+        alt="profile"
+        width={120}
+        height={120}
+        className="rounded-full object-cover"
+      />
+
+      <div className="space-y-6 text-[15px] leading-relaxed">
+        <p>Frontend Engineer 강민하입니다. 👋 </p>
+        <div className="space-y-1">
+          <p>
+            무엇을 만들든 ‘왜’를 먼저 고민하며, 더 나은 방법을 찾아가는 과정을
+            즐깁니다.
+          </p>
+          <p>
+            지식과 경험을 나누며 동료와 함께 지속적으로 성장하고, 서로의
+            인사이트를 주고받는 과정을 중시해요.
+          </p>
+        </div>
+
+        <p className="mb-1">
+          다양한 오픈소스에 기여하며 커뮤니티와 함께 성장하고 있어요.
+        </p>
+        <ul className="ml-4 space-y-1">
+          <li>
+            • TanStack (
+            <a
+              href="https://github.com/TanStack/query/pulls?q=author%3Am2na7+is%3Aclosed"
+              className="text-primary underline hover:text-blue-500 hover:transition-colors hover:duration-100"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              query
+            </a>
+            ,{' '}
+            <a
+              href="https://github.com/TanStack/form/pulls?q=author%3Am2na7+is%3Aclosed"
+              className="text-primary underline hover:text-blue-500 hover:transition-colors hover:duration-100"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              form
+            </a>
+            )
+          </li>
+          <li>
+            • toss (
+            <a
+              href="https://github.com/toss/suspensive/pulls?q=author%3Am2na7+is%3Aclosed"
+              className="text-primary underline hover:text-blue-500 hover:transition-colors hover:duration-100"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              suspensive
+            </a>
+            ,{' '}
+            <a
+              href="https://github.com/toss/react-simplikit/pulls?q=author%3Am2na7+is%3Aclosed"
+              className="text-primary underline hover:text-blue-500 hover:transition-colors hover:duration-100"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              react-simplikit
+            </a>
+            )
+          </li>
+          <li>
+            •{' '}
+            <a
+              href="https://github.com/react-hook-form/react-hook-form/pulls?q=author%3Am2na7+is%3Aclosed"
+              className="text-primary underline hover:text-blue-500 hover:transition-colors hover:duration-100"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              react-hook-form
+            </a>
+          </li>
+        </ul>
+      </div>
+    </section>
+  )
+}
